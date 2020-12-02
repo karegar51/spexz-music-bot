e72dcd2af624078e2b66d25286aea8731b9071fc
karegar51 <75231969+karegar51@users.noreply.github.com>
2020-12-02 22:22:25 +0300

    s
    k

--- index.js
+++ index.js
@@ -493,7 +493,7 @@ function play(guild, song) {
     });
 }
 
-bot.login(process.env.BOT_TOKEN);
+bot.login(NzgzNzcyMjMwMTU2OTQzNDMw.X8fmxQ.hgkm-AwxrIwoEa_WRGtMgFHbjJE);
 
 process.on("unhandledRejection", (reason, promise) => {
     try {
